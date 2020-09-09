2970fc2eec73ea94d3354bb1a943d8199941d6a7
oieho <oiehomail@gmail.com>
2020-09-09 22:51:06 +0900

    build gradle

--- src/test/java/com/test/www/web/ProfileControllerTest.java
+++ src/test/java/com/test/www/web/ProfileControllerTest.java
@@ -9,9 +9,6 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
