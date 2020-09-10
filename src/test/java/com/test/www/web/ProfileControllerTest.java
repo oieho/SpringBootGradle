f0b70c60c734f5fbca06ddd9d922d5d217d58c59
oieho <oiehomail@gmail.com>
2020-09-10 12:09:41 +0900

    profilecontroller

--- src/test/java/com/test/www/web/ProfileControllerTest.java
+++ src/test/java/com/test/www/web/ProfileControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
