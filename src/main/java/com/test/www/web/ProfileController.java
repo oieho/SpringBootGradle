4df7eb8a6036090fc5d40db572366ce89c1713f1
oieho <34331867+oieho@users.noreply.github.com>
2020-09-22 16:30:56 +0900

    Update and rename ProfileController2.java to ProfileController.java

--- src/main/java/com/test/www/web/ProfileController.java
+++ src/main/java/com/test/www/web/ProfileController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-public class ProfileController2 {
+public class ProfileController {
     private final Environment env;
 
     @GetMapping("/profile")
