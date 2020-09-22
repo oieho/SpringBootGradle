b797a356f533c8b7bace78cb71f45d860e5f4270
oieho <oiehomail@gmail.com>
2020-09-22 20:07:57 +0900

    final

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,8 +5,7 @@ function find_idle_profile()
 {
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면(즉, 40x/50x 에러 모두 포함)
-
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=real2
     else
@@ -18,7 +17,7 @@ function find_idle_profile()
       IDLE_PROFILE=real2
     else
       IDLE_PROFILE=real1
-    $fi
+    fi
 
     echo "${IDLE_PROFILE}"
 }
