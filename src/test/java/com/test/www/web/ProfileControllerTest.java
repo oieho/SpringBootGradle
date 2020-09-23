3d25533cb1abf87be285b2b7e659eccdb983587c
oieho <oiehomail@gmail.com>
2020-09-23 09:55:06 +0900

    profilecontroller2

--- src/test/java/com/test/www/web/ProfileControllerTest.java
+++ src/test/java/com/test/www/web/ProfileControllerTest.java
@@ -12,10 +12,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProfileControllerTest {
+
     @LocalServerPort
     private int port;
 
